e78ca518ed1e190ffe22235a7313140c069f5e81
martinying <5122842+martinying@users.noreply.github.com>
2025-03-06 15:09:25 -0800

    Exercise Skeleton

---
+++ src/main/java/frc/robot/subsystem/drive/SwerveDrive.java
@@ -0,0 +1,10 @@
+package frc.robot.subsystem.drive;
+
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class SwerveDrive extends SubsystemBase {
+
+    public void driveRobot(ChassisSpeeds chassisSpeeds){}
+    
+}
